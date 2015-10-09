6969f353e5ab9b8997e1451aa8dc06635e935406
melinyel922 <fingolfin922@yahoo.com>
2015-10-09 14:54:39 -0500

    Update ballista.cpp

--- ballista.cpp
+++ ballista.cpp
@@ -1,9 +1,9 @@
 class ballista: public siegeWeapon {
 	public:
-		ballista() {
-			attack = 9 + rand() % 4;//8-12
-			defense = 10;
-			health = 20;
+		ballista(int weaponLevel) {
+			attack = 90 + rand() % 40 + weaponLevel * 10;//80-120
+			defense = 5 + weaponLevel / 2;
+			health = 200 + weaponLevel * 25;
 			range = 20;
 			accuracy = 5;
 			move = 2;
